50f889091a7d9b0c720e65a21237d2828535c700
Pun <impan_1611_2544@hotmail.com>
2024-01-29 21:00:31 +0700

    first commit

---
+++ program.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+#root window
+root=Tk()
+root.title("Note Application")
+root.iconbitmap("icons/note.ico")
+root.geometry("600x600")
+root.resizable(0, 0)
+root.config(bg="#6c8099")
+
+#settings
+
+#frame
+
+root.mainloop()
