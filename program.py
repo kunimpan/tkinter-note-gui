dd50bb63cc01e14673f5e40065ce5f165bff73ec
Pun <impan_1611_2544@hotmail.com>
2024-01-30 09:49:58 +0700

    Add:frame

--- program.py
+++ program.py
@@ -9,7 +9,13 @@ root.resizable(0, 0)
 root.config(bg="#6c8099")
 
 #settings
+menu_color="#dbdadb"
+text_color="white"
 
-#frame
+#design frame
+menuFrame=Frame(root, bg=menu_color)
+textFrame=Frame(root, bg=text_color)
+menuFrame.pack(padx=5, pady=5)
+textFrame.pack(padx=5, pady=5)
 
 root.mainloop()
